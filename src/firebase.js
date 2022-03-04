ec66803fbdca0aac2f60a030ca551cabe057bec4
rushilkoundal <rushilkoundal1611@gmail.com>
2022-03-05 02:28:29 +0530

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const auth = getAuth(app)
 
 export { auth }
